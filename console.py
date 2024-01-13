6b69f79fd86056c6eba3fb3b56ba572bad55fb47
root <eslamelmishtawy@gmail.com>
2024-01-13 10:44:43 -0800

    s

--- console.py
+++ console.py
@@ -5,12 +5,22 @@ cmd interpreter
 import cmd
 
 
-class HBNBcmd(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     """
     class for air bnb clone project
     """
     prompt = '(hbnb) '
+    def do_quit(self, line):
+        """ handle quit """
+        return True
 
+    def do_EOF(self, line):
+        """ handle EOF """
+        return True
+
+    def emptyline(self):
+        """empty line do nothing  """
+        pass
 
 if __name__ == '__main__':
-    HBNBcmd().cmdloop()
+    HBNBCommand().cmdloop()
