ac897df13f3f10fa310c77f0eb259d5867c3ce46
root <eslamelmishtawy@gmail.com>
2024-01-13 03:12:47 -0800

    s

---
+++ console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+cmd interpreter
+"""
+
+
+class HBNBcmd:
+    """
+    class for air bnb clone project
+    """
+
+    def __init__():
+        """ init method """
+
+    if __name__ == '__main__':
+        HBNBcmd()
