51bc11435245f1fd817f99f8f0a1e2a7b8e2504f
root <omar.elmishtawy@gmail.com>
2024-01-14 12:14:08 -0800

    dsa

--- console.py
+++ console.py
@@ -7,7 +7,12 @@ import shlex
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-classes = ['BaseModel', 'User']
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+classes = ['BaseModel', 'User', 'State', 'City', 'Review', 'Place', 'Amenity']
 
 
 class HBNBCommand(cmd.Cmd):
